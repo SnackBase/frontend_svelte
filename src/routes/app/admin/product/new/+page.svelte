bf6eb0f471d07ab576a191966528ac6b7934c386
David Schüler <david.leon.sch@gmail.com>
2026-01-06 12:54:54 +0100

    🩹 fixed image loading for newly created products

--- src/routes/app/admin/product/new/+page.svelte
+++ src/routes/app/admin/product/new/+page.svelte
@@ -9,6 +9,7 @@
 	import { capitalizeFirstLetter } from '$lib/utils/capitalize';
 	import ButtonStyle from '$lib/styles/ButtonStyle.svelte';
 	import type { CreateProductResponse } from './+page.server';
+	import { Product } from '$lib/types/product.svelte';
 
 	let { data, form }: { data: PageData; form: CreateProductResponse } = $props();
 
@@ -17,6 +18,9 @@
 		value: type,
 		label: capitalizeFirstLetter(type)
 	}));
+
+	let product_data = $derived(form?.product);
+	let product = $derived(product_data ? new Product(product_data) : null);
 
 	// Form state
 	let productType = $state(''); //typeOptions[0].value
@@ -35,14 +39,14 @@
 		<!-- display new product that was created -->
 		<div class="flex flex-col gap-2 rounded-3xl border-2 p-4 sm:flex-row">
 			<img
-				src={form?.product?.image}
-				alt={form?.product?.name}
+				src={product?.getProxiedImageUrl()}
+				alt={product?.name}
 				class="h-32 max-w-64 rounded-2xl bg-white object-contain"
 			/>
 			<div class="flex flex-col">
-				<div class="max-w-64 truncate text-xl font-bold sm:max-w-sm">{form?.product?.name}</div>
+				<div class="max-w-64 truncate text-xl font-bold sm:max-w-sm">{product?.name}</div>
 				<div>
-					{form?.product ? formatProductPrice(form.product) : 'NaN'}
+					{product ? product.getFormattedPrice() : 'NaN'}
 				</div>
 			</div>
 		</div>
